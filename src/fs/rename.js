d45f323047a0c12013e0d03b6410facaf4c6f776
Rakhymbek Sagyn <sagrakha@gmail.com>
2022-12-04 20:17:58 +0600

    feat: add solution to rename-task

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,23 @@
+import { access, constants, rename as fileRename } from "node:fs";
+import { fileURLToPath } from "url";
+import { dirname, join } from "node:path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const __wrongFilePath = join(__dirname, "./files/wrongFilename.txt");
+const __properFilePath = join(__dirname, "./files/properFilename.md");
+
 const rename = async () => {
-    // Write your code here 
+  const failedErr = "FS operation failed";
+  access(__properFilePath, constants.F_OK, (err) => {
+    if (!err) throw Error(failedErr);
+  });
+  access(__wrongFilePath, constants.F_OK, (err) => {
+    if (err) throw Error(failedErr);
+  });
+  fileRename(__wrongFilePath, __properFilePath, (err) => {
+    if (err) throw Error(failedErr);
+  });
 };
 
 await rename();
