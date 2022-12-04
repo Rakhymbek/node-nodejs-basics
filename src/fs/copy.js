9c572572b3e1fa394d55e2dc0de005891bad9f2c
Rakhymbek Sagyn <sagrakha@gmail.com>
2022-12-04 18:37:21 +0600

    feat: add solution to copy-task

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,25 @@
+import { opendir, cp } from "node:fs";
+import { fileURLToPath } from "url";
+import { dirname, join } from "node:path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const __filesDir = join(__dirname, "./files");
+const __copyFilesDir = join(__dirname, "./files_copy");
+
 const copy = async () => {
-    // Write your code here 
+  opendir(__filesDir, (error, dir) => {
+    if (!dir) throw Error("FS operation failed");
+
+    cp(
+      __filesDir,
+      __copyFilesDir,
+      { force: false, recursive: true, errorOnExist: true },
+      (err) => {
+        if (err) throw Error("FS operation failed");
+      }
+    );
+  });
 };
 
 copy();
