14b563d54c45d768592bb43d1615bc2c7520bfbb
Rakhymbek Sagyn <sagrakha@gmail.com>
2022-12-05 00:55:27 +0600

    feat: add solution to args-task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,10 @@
 const parseArgs = () => {
-    // Write your code here 
+  const argsArr = process.argv.slice(2);
+  const parsedArgs = [];
+  argsArr.forEach((arg, index, thisArr) => {
+    index % 2 === 0 ? parsedArgs.push(`${arg} is ${thisArr[index + 1]}`) : arg;
+  });
+  console.log(parsedArgs.join(', '));
 };
 
 parseArgs();
