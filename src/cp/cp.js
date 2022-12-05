765e7c11962737d9c8f92a91844b0cfea273a81f
Rakhymbek Sagyn <sagrakha@gmail.com>
2022-12-06 01:45:49 +0600

    feat: add solution to cp-task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,15 @@
+import { fileURLToPath } from "url";
+import { dirname, join } from "node:path";
+import { fork } from "child_process";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const __scriptPath = join(__dirname, "./files/script.js");
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  fork(__scriptPath, args).on("error", (err) => {
+    if (err) throw err;
+  });
 };
 
-spawnChildProcess();
+spawnChildProcess(["someArg1", "someArg2", "anotherArg", "hello world!"]);
